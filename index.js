cc412b2615f8e73a4b66e35a809dfcb3de0afb45
sidharth budania <121341016+sidharthbudania@users.noreply.github.com>
2025-11-30 23:17:07 +0530

    Remove console logs for torrent info and size

--- index.js
+++ index.js
@@ -7,11 +7,8 @@ const download = require('./src/download');
 const torrent = torrentParser.open(process.argv[2]);
 
 console.log("Tracker URL:", torrent.announce.toString('utf8'));
-console.log("Torrent Name:", torrent.info.name);
-console.log("Info Hash:", torrentParser.infoHash(torrent).toString('hex'));
-
-const sizeBytes = torrentParser.size(torrent);
-console.log("Total Size:", sizeBytes, "bytes");
+console.log("Torrent Name:", torrent.info.name); 
+ 
 
 
 
